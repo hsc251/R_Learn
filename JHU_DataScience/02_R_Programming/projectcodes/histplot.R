e2e42a4a63cefae35f963cc769396cf1bd04ef8d
Colin Chen <88671591+hsc251@users.noreply.github.com>
2022-03-31 23:36:42 +0800

    Delete histplot.R

--- JHU_DataScience/02_R_Programming/projectcodes/histplot.R
+++
@@ -1,8 +0,0 @@
-# read off the data.table library
-library("data.table")
-
-# read in the CSV data and plot histogram
-outcome <- read.csv("outcome-of-care-measures.csv",colClasses = "character")
-hist(as.numeric(outcome[,11]), xlab = "Deaths"
-                 , main = "Hospital 30-Day Death (Mortality) Rates from Heart Attack"
-                 , col = "red")
